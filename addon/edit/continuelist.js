ca3036a6c5d8ecca34a416541075edea93b87f83
Jonathan Hart <stuajnht@users.noreply.github.com>
2017-10-18 23:19:17 +0100

    Fixed 'broken' list numbers
    E.g.:
    1. [enter]
    2. 
    3. 
    2. 
    4. 
    5. 
    
    ... becomes ...
    1. 
    2. 
    3. 
    4. 
    5. 
    6. 
    7.

--- addon/edit/continuelist.js
+++ addon/edit/continuelist.js
@@ -63,9 +63,25 @@
       var newNumber = (parseInt(startItem[3], 10) + lookAhead);
       var nextNumber = (parseInt(nextItem[3], 10));
 
+      // 'Standard' incrementing
       if ((newNumber === nextNumber) && (startIndent === nextIndent)) {
         var replaceLine = nextLine.replace(
           listRE, nextIndent + (nextNumber + 1) + nextItem[4] + nextItem[5]
+        );
+        cm.replaceRange(replaceLine, {
+          line: nextLineNumber, ch: 0
+        }, {
+          line: nextLineNumber, ch: nextLine.length
+        });
+        incrementRemainingMarkdownListNumbers(cm, pos, lookAhead + 1);
+        return;
+      }
+
+      // Remaining list numbers, which are numerically below current number
+      // i.e. Broken list numbers: 1. [enter] 2. 3. 2. 4. 5. => 1. 2. 3. 4. 5. 6. 7.
+      if ((newNumber >= nextNumber) && (startIndent === nextIndent)) {
+        var replaceLine = nextLine.replace(
+          listRE, nextIndent + (newNumber + 1) + nextItem[4] + nextItem[5]
         );
         cm.replaceRange(replaceLine, {
           line: nextLineNumber, ch: 0
