39759250a8b75ddc99426dba72bf680927032277
Jonathan Hart <stuajnht@users.noreply.github.com>
2017-10-21 19:30:11 +0100

    Moved location of comment to where it is relevant

--- addon/edit/continuelist.js
+++ addon/edit/continuelist.js
@@ -79,9 +79,9 @@
             line: nextLineNumber, ch: nextLine.length
           });
         } else {
+          if (startIndent.length > nextIndent.length) return;
           // This doesn't run if the next line immediatley indents, as it is
           // not clear of the users intention (new indented item or same level)
-          if (startIndent.length > nextIndent.length) return;
           if ((startIndent.length < nextIndent.length) && (lookAhead === 1)) return;
           skipCount += 1;
         }
